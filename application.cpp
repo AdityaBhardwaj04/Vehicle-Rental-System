21d09dacb311acd38fe27c06530f53a7ea1346ec
Niret Badgire <niretb9@gmail.com>
2023-03-31 00:31:32 +0530

    .

--- application.cpp
+++ application.cpp
@@ -116,5 +116,104 @@ void Application :: renderAddNewVehicleMenu() const{
     gotoXY(int(vehiclaTypeLabel.length()), 4);
     cin>>vehicleType;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    
+    gotoXY(int(pucExpirationDateLabel.length()),9);
+    getline(cin,pucExpirationDate);
+    vehicle * vehicle;
+    try{
+        vehicle = new Vehicle(registrationNo,VehicleType(vehicleType),seat,companyName,price,Date(pucExpirationDate));
+        this->db->addNewRecord(vehicle);
+        stringstream ss;
+        ss<<"Vehicle Id:" <<vehicle->getRecordId();
+        showDialog("Vehicle added successfully",ss.str());
+    }
+    catch(Error e){
+        showDialog(e.getMessage());
+    }
+    delete vehicle;
+}
+void Application::renderviewVehicleMenu()const{
+    string header="Enter registration no. of vehicle:";
+    string registrationNo;
+    system("clear");
+    gotoXY(0,1);
+    cout<<header;
+    gotoXY(int(header.length()),1);
+    getline(cin,registrationNo);
+    gotoXY(0,3);
+    try{
+        auto vehicle=this->db->getVehicle(registrationNo);
+        vehicle->display();
+        cout<<endl<<"Press any key to continue";
+        cin.get();
+    }
+    catch(Error e){
+        this->showDialog(e.getMessage());
+    }
+}
+void Application::renderEditVehicleMenu()const{
+    string header="Enter registration no of vehicle you want to edit:";
+    string priceLabel="Enter new price per km:";
+    string registrationNo;
+    system("clear");
+    gotoXY(0,1);
+    cout<<header;
+    gotoXY(int(header.length()),1);
+    getLine(cin, registrationNo);
+    gotoXY(0,3);
+    vehicle * modifiedVehicle;
+    try{
+        auto vehicle=this->db->getVehicle(registrationNo);
+        modifiedVehicle = new Vehicle(*vehicle);
+        vehicle->display();
+        string newPrice;
+        gotoXY(0,12);
+        cout<<priceLabel;
+        gotoXY(int(priceLabel.length()),12);
+        getline(cin,newPrice);
+        if(newPrice!=""){
+            modifiedVehicle->setPricePerKm(stod(newPrice));
+            this->db->updateRecord(modifiedVehicle);
+            this->showDialog("vehicle data updated successfully");
+        }
+    }
+    catch(Error e){
+        this->showDialog(e.getMessage());
+    }
+    delete modifiedVehicle;
+}
+void Application::renderAddNewUserMenu()const{
+    string header = "Enter details of user";
+    string userNameLabel = "Enter name of user:";
+    string usercontactLabel="Enter contact no of user:";
+    string userEmailLabel="Enter email of user(optional)";
+    string name;
+    string contact;
+    string email;
+    system("clear");
+    gotoXY(0,1);
+    cout<<header;
+    gotoXY(0,3);
+    cout<<userNameLabel;
+    gotoXY(0,4);
+    cout<<usercontactLabel;
+    gotoXY(0,5);
+    cout<<userEmailLabel;
+    gotoXY(int(userNameLabel.length()),3);
+    getline(cin,name);
+    gotoXY(int(usercontactLabel.length()),4);
+    getline(cin,contact);
+    gotoXY(int(userEmailLabel.length()),5);
+    getline(cin,email);
+    user * user;
+    try{
+        user = new User(name,contact,email);
+        this->db->addNewRecord(user);
+        stringstream ss;
+        ss<<"User Id :"<<user->getRecordId();
+        showDialog("user added successfully",ss.str());
+        }
+        catch(Error e){
+            showDialog(e.getMessage());
+        }
+        delete user;
 }
