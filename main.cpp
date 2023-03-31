d1a0adf4babe95574e79d11fba835079b8b3c6bb
Niret Badgire <niretb9@gmail.com>
2023-03-31 12:35:16 +0530

    .

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "application.h"
+#include<iostream>
+using namespace std;
+int main(int argc, const char* argv[]){
+    Application * application=new Application();
+    application->start();
+}
